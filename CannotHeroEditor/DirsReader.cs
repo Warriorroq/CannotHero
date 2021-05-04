c1b9d4dff7e73c447e4a14a365f8215bb8fca4b2
Warriorroq <Warriorroq@gmail.com>
2021-05-04 20:19:40 +0300

    Created editor and dirsreader

---
+++ CannotHeroEditor/DirsReader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+namespace CannotHeroEditor
+{
+    public class DirsReader
+    {
+        private string _dirsFolder;
+        public List<(string, string)> files;
+        public DirsReader()
+        {
+            files = new();
+            _dirsFolder = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "\\Dirs";
+            if (Directory.Exists(_dirsFolder))
+                if (ContainsSave(out var path, Directory.GetFiles(_dirsFolder), "Dirs.txt"))
+                    LoadFiles(path);
+        }
+        private void LoadFiles(string path)
+        {
+            string[] data = File.ReadAllText(path).Split("\n");
+            foreach (var raw in data)
+            {
+                var dataInput = raw.Split();
+                files.Add((dataInput[0], dataInput[1]));
+            }
+        }
+        private bool ContainsSave(out string param, string[] filesInDir, string saveName)
+        {
+            param = null;
+            foreach (var fullFileName in filesInDir)
+            {
+                int fileNameLenght = fullFileName.Length - _dirsFolder.Length - 1;
+                var fileName = fullFileName.Substring(_dirsFolder.Length + 1, fileNameLenght);
+                if (fileName.Equals(saveName))
+                {
+                    param = fullFileName;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ CannotHeroEditor/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CannotHeroEditor
+{
+    public partial class Form1 : Form
+    {
+        private DirsReader _dirsReader;
+        public Form1()
+        {
+            InitializeComponent();
+            _dirsReader = new DirsReader();
+        }
+    }
+}
